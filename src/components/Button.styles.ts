693db97a98048728574f69df0031adfbef5fb651
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-30 21:29:22 -0300

    chore(ignite-timer): add styled-components types configuration

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,14 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${(props) => css`
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white}
+
+  /* ${(props) => css`
     background-color: ${buttonVariants[props.variant]}
-  `}
+  `} */
 `;
