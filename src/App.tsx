8826f71870d241b6ae66699efaa12eaca741a8cf
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-30 22:04:10 -0300

    refactor(ignite-timer): remove button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
 
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -7,12 +6,6 @@ import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   );
