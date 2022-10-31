5c898d5390b1ae12dc646aadc7de173e48667be6
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-30 22:21:28 -0300

    feat(ignite-timer): add route layout

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,4 +1,4 @@
-export default function History() {
+export function History() {
   return (
     <div>History</div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export function Home() {
   return (
     <div>Home</div>
   );
