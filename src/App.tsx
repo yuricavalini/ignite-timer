12810a367ac38e8d21fd34801669e12802ba9ca7
Yuri Cavalini <yuricavalini@gmail.com>
2022-10-30 22:09:36 -0300

    feat(ignite-timer): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -6,6 +8,9 @@ import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
